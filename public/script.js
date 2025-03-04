dd1a89e2de6ce2f889670e83a12c024547df1994
Panowar <p.manis13@gmail.com>
2025-03-04 19:12:25 +0200

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 document.getElementById("downloadBtn").addEventListener("click", () => {
-    fetch('https://my-video-site-d46y.onrender.com')
+    fetch('https://my-video-site-d46y.onrender.com/get-video')
         .then(response => response.json())
         .then(data => {
             if (data.fileUrl) {
